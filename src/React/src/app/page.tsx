4444523f05519b7b82f91c42755c95709e5c2fb3
bjarne-klago <114390366+bjarne-klago@users.noreply.github.com>
2023-06-26 16:56:41 +0200

    Erster versuch für TextWithPicturew für die homeseite

--- src/React/src/app/page.tsx
+++ src/React/src/app/page.tsx
@@ -1,11 +1,12 @@
 
 import Head from 'next/head'
 import styles from '../app/page.module.css'
+import TextWithPicture from '../../public/components/TextWithPicture'
 
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <TextWithPicture />
     </main>
   )
 }
