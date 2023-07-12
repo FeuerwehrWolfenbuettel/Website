3fb0c0f129585b851f4617203e422a2da70af88c
Bjarne Klages <114390366+bjarne-klago@users.noreply.github.com>
2023-07-12 19:54:08 +0200

    Update TextWithPicture.js

--- src/React/public/components/TextWithPicture.js
+++ src/React/public/components/TextWithPicture.js
@@ -1,13 +1,35 @@
 import styles from '../css_for_components/TextWithPicture.css'
+import React, { useEffect, useState } from 'react';
+import styles from '../css_for_components/TextWithPicture.css';
 import Image from 'next/image';
 
 const TextWithPicture = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch('https://api.example.com/data'); // Replace with your API endpoint
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    if (!data) {
+        return <div>Loading...</div>; // Add a loading state while data is being fetched
+    }
+
     return (
         <div className='content'>
             <div className='spacer-before'></div>
             <div className='content-with-picture'>
                 <Image
-                    src="/images/Gesamtbild-Feuerwehr-Wolfenbüttel.jpg"
+                    src={data.image} // Replace with the attribute from your API response for the image URL
                     width={500}
                     height={300}
                     alt='Logo'
@@ -19,21 +41,12 @@ const TextWithPicture = () => {
                             <div className='header-stripe'></div>
                         </div>
                         <div className='header'>
-                            <span>150 jähriges</span>
-                            <span>Jubiläum</span>
+                            <span>{data.title}</span> {/* Replace with the attribute from your API response for the title */}
+                            <span>{data.subtitle}</span> {/* Replace with the attribute from your API response for the subtitle */}
                         </div>
                     </div>
                     <div className='flowing-text'>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                            diam nonumy eirmod tempor invidunt ut labore et dolore
-                            magna aliquyam erat, sed diam voluptua. At vero eos et
-                            accusam et justo duo dolores et ea rebum. Stet clita kasd
-                            gubergren, <br /> <br />
-                            
-                            
-                            no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                            diam nonumy eirmod tempor invidunt ut labore et </p>
+                        <p>{data.content}</p> {/* Replace with the attribute from your API response for the content */}
                     </div>
                     <button className='continue-button'>Weiter lesen</button>
                 </div>
