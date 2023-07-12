c32387eff98f4ecb9016982108e7ae9204659911
Bjarne Klages <114390366+bjarne-klago@users.noreply.github.com>
2023-07-12 21:37:09 +0200

    .

--- src/React/src/app/page.tsx
+++ src/React/src/app/page.tsx
@@ -1,6 +1,3 @@
-
-import Head from 'next/head'
-import styles from '../app/page.module.css'
 import TextWithPicture from '../../public/components/TextWithPicture'
 
 export default function Home() {
