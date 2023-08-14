54791c3e44232a6d781f9cec2a36a872d65ab73e
Vincent Kurby <v.kurby@ostfalia.de>
2023-08-14 23:35:51 +0200

    addd

---
+++ docu/src/API-req.md
@@ -0,0 +1,8 @@
+# API Endpunkte
+
+apiURL/articles                             (GET, POST)
+apiURL/articles/{id} z.B. apiURL/articles/1 (GET, PUT, DELETE)
+apiURL/vehicles                             (GET, POST)
+apiURL/vehicles/{id} z.B. apiURL/vehicles/1 (GET, PUT, DELETE)
+
+# Examples
--- src/React/src/app/about/page.tsx
+++ src/React/src/app/about/page.tsx
@@ -1,11 +1,30 @@
-
-import Head from 'next/head'
-import styles from '../app/page.module.css'
+import './card.css'
 
 export default function About() {
   return (
     <main>
       <h1>Über uns</h1>
+      <Card/>
+      <Card/>
+      <Card/>
     </main>
   )
+}
+
+function Card() {
+
+  return (
+
+    <div className="card">
+    <div className="info">
+      <div className="headline">
+        <h1>Einsatzabteilung</h1>
+      </div>
+    </div>
+    <div className="picture">
+      <img src="https://i.ibb.co/yXcM07d/garreth-paul-3-Qo-ABp-ZPGqs-unsplash.jpg" alt="Beschreibung des Bildes"/>
+    </div>
+  </div>
+  )
+
 }
