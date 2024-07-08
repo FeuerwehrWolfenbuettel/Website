8722888a8973c8979b2fa91a959f99d79a6f0784
Bjarne Klages <114390366+bjarne-klago@users.noreply.github.com>
2024-07-08 21:16:21 +0200

    UPdate Responsive Webite

--- src/React/src/app/page.tsx
+++ src/React/src/app/page.tsx
@@ -1,5 +1,6 @@
 import "./page.scss";
 import AnimatedText from '../components/Animatedtext/AnimatedText'
+import ArticlePreView from "@/components/ArticlePreView/ArticlePreView";
 
 export default function Home() {
   return (
@@ -7,13 +8,8 @@ export default function Home() {
       <div className="background-image">
         <AnimatedText />
       </div>
-      <section className="content">
-        <h1>Willkommen auf unserer Landing Page</h1>
-        <p>Scrollen Sie nach unten, um mehr zu erfahren.</p>
-      </section>
       <section className="additional-content">
-        <h2>Mehr über uns</h2>
-        <p>Hier steht mehr Information über uns...</p>
+        <ArticlePreView />
       </section>
     </main>
   );
---
+++ src/React/src/components/ArticlePreView/ArticlePreView.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import styles from './ArticlePreView.module.scss';
+
+const ArticlePreView = () => {
+
+    return (
+        <div className={styles.content}>
+            <div className={styles.imageContainer}>
+                <img 
+                    src="/images/Gesamtbild-Feuerwehr-Wolfenbüttel.jpg" 
+                    alt="Bild" 
+                />
+            </div>
+            <div className={styles.rightSite}>
+                <div className={styles.text}>
+                    <div className={styles.heading}>
+                        <div className={styles.headingStripe}></div>
+                        <h1 className={styles.hedingFont}>Überschrift</h1>
+                    </div>
+                    <div className={styles.flowingText}>
+                        <p className={styles.flowingTextFont}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                    </div>
+                </div>
+                <div className={styles.buttonContainer}>
+                    <button className={styles.continueButton}>Weiter lesen</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ArticlePreView;
